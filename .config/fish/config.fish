cad465792746de3fc67129f624d6f3622ec2343f
Simon Gauvin <gauvinsimon@gmail.com>
2024-03-04 00:33:02 -0500

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,22 @@
+set fish_greeting
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -gx EDITOR nvim
+
+fish_add_path --path ~/.local/share/bob/nvim-bin
+
+# Zoxide
+zoxide init --cmd cd fish | source
+
+function ya
+	set tmp (mktemp -t "yazi-cwd.XXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		cd "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
+starship init fish | source
