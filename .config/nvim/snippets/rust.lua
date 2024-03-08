63480765da5ac09a2e4428dccf8ae0fb9355ae37
Simon Gauvin <gauvinsimon@gmail.com>
2024-03-08 12:56:36 -0500

    Add pub fn snippet for rust files

--- .config/nvim/snippets/rust.lua
+++ .config/nvim/snippets/rust.lua
@@ -5,29 +5,34 @@ local t = ls.t
 local c = ls.choice_node
 local fmt = require("luasnip.extras.fmt").fmt
 
-local return_type_choices = {
-  t(""),
-  fmt(" -> {}", { i(1, "") }),
-  fmt(" -> Result<{}, {}>", { i(1, ""), i(2, "") }),
-  fmt(" -> Option<{}>", { i(1, "") }),
-}
+local function return_type_choices()
+  return {
+    t(""),
+    fmt(" -> {}", { i(1, "") }),
+    fmt(" -> Result<{}, {}>", { i(1, ""), i(2, "") }),
+    fmt(" -> Option<{}>", { i(1, "") }),
+  }
+end
 
-local function_snip = s(
-  "fn",
-  fmt(
-    [[
+local function function_fmt_str()
+  return [[
 fn {}({}){} {{
     {}
 }}
-  ]],
-    {
-      i(1, "foo"), -- name
-      i(2, ""), -- parameters
-      c(3, return_type_choices), -- return type
-      i(0, "todo!();"), -- body
-    }
-  )
-)
+  ]]
+end
+
+local function function_nodes()
+  return {
+    i(1, "foo"), -- name
+    i(2, ""), -- parameters
+    c(3, return_type_choices()), -- return type
+    i(0, "todo!();"), -- body
+  }
+end
+
+local function_snip = s("fn", fmt(function_fmt_str(), function_nodes()))
+local pub_function_snip = s("pfn", fmt("pub " .. function_fmt_str(), function_nodes()))
 
 local enum_snip = s(
   "enum",
@@ -52,5 +57,6 @@ local enum_snip = s(
 
 return {
   function_snip,
+  pub_function_snip,
   enum_snip,
 }, {}
