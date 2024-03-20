77247e51dcedadc51f241d2ac60bc765212739c0
Simon Gauvin <gauvinsimon@gmail.com>
2024-03-19 20:08:24 -0400

    Remove treesitter context

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -204,9 +204,9 @@ require("lazy").setup({
           end
           vim.schedule(function()
             gs.prev_hunk()
-          end, { desc = "Previous Hunk" })
+          end)
           return "<Ignore>"
-        end, { expr = true })
+        end, { expr = true, desc = "Previous Hunk" })
 
         -- Actions
         map("n", "<leader>hs", gs.stage_hunk, { desc = "[H]unk [S]tage" })
@@ -865,28 +865,28 @@ require("lazy").setup({
     "tpope/vim-fugitive",
   },
 
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    config = function()
-      vim.api.nvim_set_hl(0, "TreesitterContext", { fg = "#000000", bg = "#000000" })
-      vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { fg = "#f38ba8", bg = "#000000" })
-
-      require("treesitter-context").setup({
-        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-        max_lines = 1, -- How many lines the window should span. Values <= 0 mean no limit.
-        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-        line_numbers = true,
-        multiline_threshold = 1, -- Maximum number of lines to show for a single context
-        trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-        mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
-        -- Separator between context and content. Should be a single character string, like '-'.
-        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-        separator = "─",
-        zindex = 20, -- The Z-index of the context window
-        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-      })
-    end,
-  },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter-context",
+  --   config = function()
+  --     vim.api.nvim_set_hl(0, "TreesitterContext", { fg = "#000000", bg = "#000000" })
+  --     vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { fg = "#f38ba8", bg = "#000000" })
+  --
+  --     require("treesitter-context").setup({
+  --       enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+  --       max_lines = 1, -- How many lines the window should span. Values <= 0 mean no limit.
+  --       min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+  --       line_numbers = true,
+  --       multiline_threshold = 1, -- Maximum number of lines to show for a single context
+  --       trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+  --       mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+  --       -- Separator between context and content. Should be a single character string, like '-'.
+  --       -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+  --       separator = "─",
+  --       zindex = 20, -- The Z-index of the context window
+  --       on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+  --     })
+  --   end,
+  -- },
 
   { -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
